6b7324563a0a0e6f4cff79c950d3e39ad353ea31
marclllaks <marcelllakos@gmail.com>
2020-05-29 01:32:16 +0200

    ki lett pucolva a kód

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,9 +1,6 @@
 (ns app.views
   (:require [reagent.core :refer [atom]]))
 
-(defonce state (atom {0 1}))
-(defonce doboz (atom {}))
-
 (def feladatok (atom []))
 
 (defn atom-input [value]
@@ -11,33 +8,14 @@
            :value @value :placeholder "Ide írj..."
            :on-change #((reset! value (-> % .-target .-value)))}])
 
-
-(defn torles [valt]
-  (reset! valt ""))
-
-
 (defn mentes [szoveg]
-
       (swap! feladatok conj {:nev @szoveg :pipa false})
-      (torles szoveg))
-    ; (reset! szoveg "")))
-
-(defn bepipal [id]
-  (if (= 1 (@doboz id))
-    (swap! doboz update-in (constantly @id) (constantly 0))
-    (swap! doboz update-in (constantly @id) (constantly 1))))
-
-(defn elvegezve []
-  (if (= 1 (get @state :doboz))
-    (swap! state update-in [:doboz] dec)
-    (swap! state update-in [:doboz] inc)))
+      (reset! szoveg ""))
 
 (defn feladattorol
       [param]
       (reset! feladatok (into [] (concat (subvec @feladatok 0 param)
                                          (subvec @feladatok (inc param))))))
-
-
 
 (defn app []
   (let [valtozo (atom "")]
@@ -52,7 +30,6 @@
        [:button {:on-click #(when (not (empty? @valtozo)) (mentes valtozo))}
               "Mentés"]]
       [:p "Feladatok száma: " (count @feladatok)]
-
       (for [i (range (count @feladatok))]
         (let [feladat (nth @feladatok i)]
           [:p {:class [(when (feladat :pipa) "athuzva")]} (feladat :nev)
